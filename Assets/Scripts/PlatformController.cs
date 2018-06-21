1ca3a0f2fd4f8a34e368b9ec158420cf65fab377
Zachary Geier <zdgeier@gmail.com>
2018-06-20 22:19:59 -0400

    Initial commit

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour {
+    public GameObject[] cylinders;
+    int currentCylinder = 4;
+    bool started = false;
+    
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.R) && !started)
+        {
+            startRepeating();
+            started = true;
+        }
+    }
+
+    void startRepeating()
+    {
+        InvokeRepeating("DropPlatform", 2.0f, 5f);
+    }
+
+    void DropPlatform()
+    {
+        if (currentCylinder >= 0)
+        {
+            StartCoroutine(MoveOverSeconds(cylinders[currentCylinder], new Vector3(0.0f, -10f, 0f), 5f));
+            currentCylinder--;
+        }
+        else
+        {
+            CancelInvoke();
+        }
+    }
+
+    public IEnumerator MoveOverSpeed(GameObject objectToMove, Vector3 end, float speed)
+    {
+        // speed should be 1 unit per second
+        while (objectToMove.transform.position != end)
+        {
+            objectToMove.transform.position = Vector3.MoveTowards(objectToMove.transform.position, end, speed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    public IEnumerator MoveOverSeconds(GameObject objectToMove, Vector3 end, float seconds)
+    {
+        float elapsedTime = 0;
+        Vector3 startingPos = objectToMove.transform.position;
+        while (elapsedTime < seconds)
+        {
+            objectToMove.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        objectToMove.transform.position = end;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+    public Rigidbody rb;
+    public float speed;
+    public float vThrust;
+
+    [SyncVar]
+    public char currentInput = ' ';
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        // DO NOT SEND CMDS ON EVERY FRAME IDIOT
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdJump();
+        }
+
+        if (moveHorizontal != 0 || moveVertical != 0)
+        {
+            CmdMove(moveHorizontal, moveVertical);
+        }        
+    }
+
+    [Command]
+    void CmdMove(float moveHorizontal, float moveVertical)
+    {
+        Vector3 movement = new Vector3(moveVertical, 0.0f, -moveHorizontal);
+
+        rb.AddTorque(movement * speed);
+    }
+
+    [Command]
+    void CmdJump()
+    {
+        rb.AddForce(new Vector3(0, 1, 0) * vThrust);
+    }
+}
